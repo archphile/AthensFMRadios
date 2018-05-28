ed578d094865bed9d2d1259559cc4719986b9919
Archphile <mechmg93@gmail.com>
2018-05-28 12:08:26 +0300

    updated script

--- archphile-script/arf.sh
+++ archphile-script/arf.sh
@@ -6,3 +6,4 @@ mv AthensFMRadios-master /var/lib/mpd/music/webradio-athens
 chown -R mpd:audio /var/lib/mpd/music/webradio-athens/
 rm master.zip
 rm -r /var/lib/mpd/music/webradio-athens/archphile-script
+rm arf.sh
