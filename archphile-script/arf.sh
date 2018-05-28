4f7bb58f23dda9abddb21c1eac3afdd683f69f7b
Archphile <mechmg93@gmail.com>
2018-05-28 12:05:41 +0300

    initial commit

---
+++ archphile-script/arf.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget https://github.com/archphile/AthensFMRadios/archive/master.zip
+unzip master.zip
+mv AthensFMRadios-master /var/lib/mpd/music/webradio-athens
+chown -R mpd:audio /var/lib/mpd/music/webradio-athens/
+rm master.zip
+rm -r /var/lib/mpd/music/webradio-athens/archphile-script
